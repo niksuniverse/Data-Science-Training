7987e6d5535fa00ac189e0bf24d2e9aded05385d
Nikitha Y S <137302173+niksuniverse@users.noreply.github.com>
2025-07-25 15:23:22 +0530

    myModules updated

--- JULY 25/myModule.py
+++ JULY 25/myModule.py
@@ -1,1 +1,32 @@
 
+def add(a, b):
+    return a + b
+
+def is_even(n):
+    return n % 2 == 0
+  
+def mul(n,m):
+    return n*m
+  
+def sub(a,b):
+  if a>b:
+    return a-b
+  else:
+    return b-a
+
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def reverse_string(s):
+    return s[::-1]
+
+def get_max(lst):
+    return max(lst)
+
+def is_palindrome(s):
+    s = s.lower().replace(" ", "")
+    return s == s[::-1]
+
+
+def average(numbers):
+    return sum(numbers) / len(numbers) if numbers else 0
