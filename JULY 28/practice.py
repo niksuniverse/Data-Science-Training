3e55a74a98fd192efaa7106ded4512d191860f08
Nikitha Y S <137302173+niksuniverse@users.noreply.github.com>
2025-07-28 13:02:13 +0530

    numpy pandas

--- JULY 28/practice.py
+++ JULY 28/practice.py
@@ -1,6 +1,17 @@
+
 import pandas as pd
+
 db=pd.read_csv('employee.csv')
 print(db)
 
 db.to_csv('updated_emp.csv',index=False)
 print("Datas updated to updated_emp csv file")
+
+import json
+with open('data.json','r') as f:
+    data=json.load(f)
+print("JSON Read:\n",data)
+
+with open('Output.json','w') as f:
+    json.dump(data,f,indent=4)
+print("JSON Written to output.json")
